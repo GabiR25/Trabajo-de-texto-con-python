745d00071241f4ffd9bf35a5f4f0f20410090515
GabiR <gabrielr16000@gmail.com>
2023-06-28 18:23:43 -0300

    Trabajo-de-texto-con-python

---
+++ ejparcial2.py
@@ -0,0 +1,19 @@
+archivo = open('ejparcial2.txt','r',encoding = 'utf-8')
+
+lineas = archivo.read()
+lineas = lineas.lower()
+palabras = lineas.split()
+
+diccionario = {}
+for palabra in palabras:
+    if palabra in diccionario:
+        diccionario[palabra] += 1
+    else:
+        diccionario[palabra] = 1
+
+for palabra in diccionario:
+    frecuencia = diccionario[palabra]
+    if frecuencia > 1:
+        print(f'La palabra {palabra} aparece {frecuencia} veces')
+    else:
+        print(f'La palabra {palabra} aparece {frecuencia} vez')
