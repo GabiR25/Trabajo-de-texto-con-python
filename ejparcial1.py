3fbd9b2a30e128d457400e2d072818265d72975b
GabiR <gabrielr16000@gmail.com>
2023-06-28 17:47:59 -0300

    Trabajo-de-texto-con-python

---
+++ ejparcial1.py
@@ -0,0 +1,9 @@
+cont = 0
+archivo = open('ejparcial1.txt' , 'r', encoding = 'utf-8')
+
+for palabra in archivo.read().split(' '):
+    cont += 1
+
+archivo.close()
+
+print(f'En el texto, hay una cantidad de {cont} palabras')
